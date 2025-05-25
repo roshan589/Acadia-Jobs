2b9888a69dcb45cf0dfa4fc6c11acdd2aa603cbf
roshan-alt <roshanadhikari0420@gmail.com>
2025-05-25 19:10:53 -0300

    Modified and Adding Files

--- loginSignup/apps.py
+++ loginSignup/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class LoginsignupConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'loginSignup'
+    def ready(self):
+        import loginSignup.signals  # import signals so Django registers them
+
+
+
+
---
+++ loginSignup/signals.py
@@ -0,0 +1,40 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.contrib.sites.models import Site
+from django.conf import settings
+
+from .models import CreateJob
+from loginSignup.models import CustomUser  # adjust if needed
+
+@receiver(post_save, sender=CreateJob)
+def send_job_email(sender, instance, created, **kwargs):
+    if created:
+        current_site = Site.objects.get_current()
+        job_url = f"https://{current_site.domain}/jobs/{instance.id}/"
+
+        subject = f"New Job Posted: {instance.title}"
+        from_email = settings.DEFAULT_FROM_EMAIL
+
+        users = CustomUser.objects.all()
+        recipient_list = [user.email for user in users if user.email]
+
+        for email in recipient_list:
+            text_content = (
+                f"New job alert!\n\n"
+                f"Title: {instance.title}\n"
+                f"Company: {instance.companyName}\n"
+                f"Position: {instance.position}\n\n"
+                f"{instance.description}\n\n"
+                f"View Job: {job_url}"
+            )
+
+            html_content = render_to_string("emails/newJobPost.html", {
+                "job": instance,
+                "job_url": job_url
+            })
+
+            msg = EmailMultiAlternatives(subject, text_content, from_email, [email])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
