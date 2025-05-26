587e88b82150ff9c01c8bbd435ce7b88d0646e67
roshan-alt <roshanadhikari0420@gmail.com>
2025-05-26 14:01:11 -0300

    Edited the html file for mail

--- employementProject/settings.py
+++ employementProject/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'loginSignup',
     'tailwind',
-    'theme',
+    # 'theme',
     'django.contrib.sites',
     'django_browser_reload',
 
