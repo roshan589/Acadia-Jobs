de6641aae4f7ab0968019ecaac4bb092f57a518f
roshan-alt <roshanadhikari0420@gmail.com>
2025-05-28 21:59:12 -0300

    For New Feature

--- employementProject/settings.py
+++ employementProject/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
