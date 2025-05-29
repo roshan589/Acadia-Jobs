d82e21489f435c08805460a66349dcc88b385de3
roshan-alt <roshanadhikari0420@gmail.com>
2025-05-29 16:56:17 -0300

    Adding Parent feature

--- employementProject/settings.py
+++ employementProject/settings.py
@@ -56,8 +56,8 @@ DOMAIN = "3.99.152.118"
 SITE_ID = 1
 TAILWIND_APP_NAME = 'theme' # This is the name of the app that will be used to generate the tailwind files
 INTERNAL_IPS = ['127.0.0.1']
-# NPM_BIN_PATH = "/usr/bin/npm"
-NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+NPM_BIN_PATH = "/usr/bin/npm"
+# NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
