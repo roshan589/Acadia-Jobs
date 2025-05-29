f00228844de7e4bcf5e8660f994a2b57edd826fb
roshan-alt <roshanadhikari0420@gmail.com>
2025-05-29 19:16:58 -0300

    Adding Parent feature

--- loginSignup/middleware.py
+++ loginSignup/middleware.py
@@ -7,7 +7,7 @@ class CheckParentExpiryMiddleware(MiddlewareMixin):
     def process_request(self, request):
         if request.user.is_authenticated:
             if getattr(request.user, 'user_type', None) == 'parent':
-                if hasattr(request.user, 'isParentAccountValid') and not request.user.is_parent_account_valid():
+                if hasattr(request.user, 'isParentAccountValid') and not request.user.isParentAccountVaild():
                     logout(request)
                     messages.error(request, "Your parent account has expired.")
                     return redirect('login')
