d1f6684a9341f64de888a318447135286f0ece79
roshan-alt <roshanadhikari0420@gmail.com>
2025-05-28 23:11:16 -0300

    Pass Reset

--- loginSignup/urls.py
+++ loginSignup/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path("verify-account/", views.verify_email, name="verify_email"),
     path('change-password/', views.passChangeView, name='password_change'),
     path('forgot-password/', views.password_reset_request, name='password_reset'),
-    path('password-reset-done/', views.password_reset_done_custom, name='password_email'),
+    path('password-reset-done/', views.password_email, name='password_email'),
 
     path('reset/<uidb64>/<token>/', views.password_reset_confirm, name='password_reset_confirm'),
     path('logout/', views.logoutView, name='logout'),
